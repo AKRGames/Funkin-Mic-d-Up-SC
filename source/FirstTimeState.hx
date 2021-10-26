ed744c81f521d4ec49cbb7800e2df3566ba09178
AKRGames <65655953+AKRGames@users.noreply.github.com>
2021-10-26 07:01:28 +0200

    First time?

--- source/FirstTimeState.hx
+++ source/FirstTimeState.hx
@@ -14,13 +14,21 @@ import Discord.DiscordClient;
 class FirstTimeState extends MusicBeatState
 {
 
-	var sinMod:Float = 0;
+	// var sinMod:Float = 0;
+	// var txt:FlxText = new FlxText(0, 360, FlxG.width,
+		// "WARNING:\nFNF: Mic'd Up may potentially trigger seizures for people with photosensitive epilepsy.Viewer discretion is advised.\n\n"
+		// + "FNF: Mic'd Up is a non-profit modification, aimed for entertainment purposes, and wasn't meant to be an attack on Ninjamuffin99"
+		// + " and/or any other modmakers out there. I was not aiming for replacing what Friday Night Funkin' was, won't be aiming for that and never"
+		// + " will be aiming for that. It was made for fun and from the love for the game itself. All of the comparisons between this and other mods are purely coincidental, unless stated otherwise.\n\n"
+		// + "Now with that out of the way, I hope you'll enjoy this FNF mod.\nFunk all the way.\nPress ENTER to proceed",
+		// 32);
+        
 	var txt:FlxText = new FlxText(0, 360, FlxG.width,
-		"WARNING:\nFNF: Mic'd Up may potentially trigger seizures for people with photosensitive epilepsy.Viewer discretion is advised.\n\n"
-		+ "FNF: Mic'd Up is a non-profit modification, aimed for entertainment purposes, and wasn't meant to be an attack on Ninjamuffin99"
-		+ " and/or any other modmakers out there. I was not aiming for replacing what Friday Night Funkin' was, won't be aiming for that and never"
-		+ " will be aiming for that. It was made for fun and from the love for the game itself. All of the comparisons between this and other mods are purely coincidental, unless stated otherwise.\n\n"
-		+ "Now with that out of the way, I hope you'll enjoy this FNF mod.\nFunk all the way.\nPress ENTER to proceed",
+		"Heads up!\nFriday Night Funkin: The Stoopid Mod contains a lot of stoopid memes. Viewer discretion is advised.\n\n"
+		+ "The Stoopid Mod is cool. You should agree."
+		+ "STOOPID GUY IS NOT LITTLE MAN ON STEROIDS. That's the only argument I can make."
+		+ "Anyway, shoutout to Verwex for making this engine.\n\n"
+		+ "Thanks for reading. I hope you enjoy this mod.\n(you should also play little man and bob mod)\nPress ENTER to proceed.",
 		32);
 
 	override function create()
@@ -36,10 +44,10 @@ class FirstTimeState extends MusicBeatState
 		super.create();
 	}
 
-	override function update(elapsed:Float)
-	{
-		sinMod += 0.007;
-		txt.y = Math.sin(sinMod)*60+100;
+	// override function update(elapsed:Float)
+	// {
+		// sinMod += 0.007;
+		// txt.y = Math.sin(sinMod)*60+100;
 
 		if (controls.ACCEPT)
 		{
